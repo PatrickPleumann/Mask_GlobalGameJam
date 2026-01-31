8b7497906e906eb98ba39eb9328e2f81e04a0268
Jan Schmitz <mail@jan-schmitz.dev>
2026-01-31 12:21:06 +0100

    Feature: add start, credits, quit to main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    [DisallowMultipleComponent]
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_transitionDelay = 0.5f;
+
+        private Coroutine m_activeCoroutine = null;
+
+        public void StartGame()
+        {
+            if (m_activeCoroutine == null)
+            {
+                m_activeCoroutine = StartCoroutine(StartGameAsync());
+            }
+            
+        }
+
+        public void QuitGame()
+        {
+            if (m_activeCoroutine == null)
+            {
+                m_activeCoroutine = StartCoroutine(QuitGameAsync());
+            }
+        }
+
+        private IEnumerator StartGameAsync()
+        {
+            yield return new WaitForSeconds(m_transitionDelay);
+            int index = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(index + 1);
+        }
+
+        private IEnumerator QuitGameAsync()
+        {
+            yield return new WaitForSeconds(m_transitionDelay);
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
