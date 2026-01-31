473598b4bff9a277a6e553f5bc2bfab2442be8b4
Jan Schmitz <mail@jan-schmitz.dev>
2026-01-31 14:08:40 +0100

    Refactor: implement player sprites

--- Assets/Scripts/Player/PlayerRepresentation.cs
+++ Assets/Scripts/Player/PlayerRepresentation.cs
@@ -1,12 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Player
 {
+    [System.Serializable]
+    public class PlayerSprite
+    {
+        public Sprite Sprite => m_sprite;
+        public EPlayerType PlayerType => m_playerType;
+
+        [SerializeField]
+        private Sprite m_sprite = null;
+        [SerializeField]
+        private EPlayerType m_playerType = EPlayerType.NORMAL;
+    }
+
     [DisallowMultipleComponent]
     public class PlayerRepresentation : MonoBehaviour
     {
+        [SerializeField]
+        private List<PlayerSprite> m_sprites = null;
+        [SerializeField]
+        private Sprite m_fallback = null;
+
         private SpriteRenderer m_spriteRenderer = null;
         private PlayerState m_playerState = null;
+        private Dictionary<EPlayerType, Sprite> m_spritePerType = new Dictionary<EPlayerType, Sprite>();
 
         private void Awake()
         {
@@ -14,22 +33,27 @@ namespace Player
             m_playerState = GetComponentInParent<PlayerState>();
 
             m_playerState.OnPlayerTypeChanged += UpdateRepresentation;
+
+            foreach (PlayerSprite playerSprite in m_sprites)
+            {
+                m_spritePerType[playerSprite.PlayerType] = playerSprite.Sprite;
+            }
         }
 
         private void UpdateRepresentation(EPlayerType _previous, EPlayerType _current)
         {
             PlayerProperties properties = m_playerState.CurrentProperties;
-            m_spriteRenderer.transform.localScale = new Vector3(properties.Width, properties.Height, 1f);
             m_spriteRenderer.transform.position = m_playerState.transform.position + new Vector3(0f, properties.Height / 2f, 0f);
-            m_spriteRenderer.color = _current switch
+            if (m_spritePerType.ContainsKey(_current))
             {
-                EPlayerType.NORMAL => Color.white,
-                EPlayerType.WIDE => Color.green,
-                EPlayerType.HEAVY => Color.red,
-                EPlayerType.NARROW => Color.blue,
-                EPlayerType.LIQUID => Color.cyan,
-                _ => Color.white,
-            };
+                m_spriteRenderer.sprite = m_spritePerType[_current];
+                m_spriteRenderer.transform.localScale = Vector3.one;
+            }
+            else
+            {
+                m_spriteRenderer.sprite = m_fallback;
+                m_spriteRenderer.transform.localScale = new Vector3(properties.Width, properties.Height, 1f);
+            }
         }
     }
 }
