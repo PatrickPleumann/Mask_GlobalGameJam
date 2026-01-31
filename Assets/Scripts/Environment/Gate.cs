1ceb0b3e3d61495dc6c2e15a76298995f5dee92b
Jan Schmitz <mail@jan-schmitz.dev>
2026-01-31 16:10:04 +0100

    Refactor: add gate which appears/disappers

---
+++ Assets/Scripts/Environment/Gate.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Environment
+{
+    [DisallowMultipleComponent]
+    public class Gate : AControllable
+    {
+        [SerializeField]
+        private bool m_initialVisibility = true;
+
+        private void Awake()
+        {
+            this.gameObject.SetActive(m_initialVisibility);
+        }
+
+        public override void Activate()
+        {
+            this.gameObject.SetActive(!m_initialVisibility);
+        }
+
+        public override void Deactivate()
+        {
+            this.gameObject.SetActive(m_initialVisibility);
+        }
+    }
+}
